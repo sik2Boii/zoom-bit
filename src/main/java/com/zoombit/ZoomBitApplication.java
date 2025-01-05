885f3564b5630a026aa4b561ae6480d78597fee1
Junsik <junsik213@naver.com>
2025-01-05 23:43:39 +0900

    프로젝트 생성

---
+++ src/main/java/com/zoombit/ZoomBitApplication.java
@@ -0,0 +1,13 @@
+package com.zoombit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomBitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomBitApplication.class, args);
+    }
+
+}
