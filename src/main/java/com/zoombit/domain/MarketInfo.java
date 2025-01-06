51e05f004f138a50aadc9b65a59159d842897fee
Junsik <junsik213@naver.com>
2025-01-07 00:43:12 +0900

    빗썸 API 호출 테스트

---
+++ src/main/java/com/zoombit/domain/MarketInfo.java
@@ -0,0 +1,30 @@
+package com.zoombit.domain;
+
+public class MarketInfo {
+
+    private String market;
+    private String korean_name;
+    private String english_name;
+
+    public String getMarket() {
+        return market;
+    }
+
+    public String getKorean_name() {
+        return korean_name;
+    }
+
+    public String getEnglish_name() {
+        return english_name;
+    }
+
+    @Override
+    public String toString() {
+        return "MarketInfo{" +
+                "market='" + market + '\'' +
+                ", korean_name='" + korean_name + '\'' +
+                ", english_name='" + english_name + '\'' +
+                '}';
+    }
+
+}
--- src/test/java/com/zoombit/ZoomBitApplicationTests.java
+++ src/test/java/com/zoombit/ZoomBitApplicationTests.java
@@ -1,8 +1,15 @@
 package com.zoombit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zoombit.domain.MarketInfo;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -10,6 +17,23 @@ class ZoomBitApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void BithumbApiTest() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+        String url = "https://api.bithumb.com/v1/market/all?isDetails=false";
+        String response = restTemplate.getForObject(url, String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<MarketInfo> marketInfoList = objectMapper.readValue(response, new TypeReference<List<MarketInfo>>() {});
+
+        // 결과 출력
+        System.out.println("##### marketInfoList.size(): " + marketInfoList.size());
+        int idx = 1;
+        for (MarketInfo marketInfo : marketInfoList) {
+            System.out.println("idx: " + idx + " == " + marketInfo);
+            idx++;
+        }
     }
 
 }
