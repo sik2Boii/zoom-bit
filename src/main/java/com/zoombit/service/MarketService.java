a7521df53fb588047d0cc85fffa343f9aa896df6
Junsik <junsik213@naver.com>
2025-01-08 22:30:52 +0900

    에러 로그 추가

--- src/main/java/com/zoombit/service/MarketService.java
+++ src/main/java/com/zoombit/service/MarketService.java
@@ -1,11 +1,14 @@
 package com.zoombit.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zoombit.dto.MarketDTO;
 import com.zoombit.domain.Markets;
+import com.zoombit.dto.MarketDTO;
 import com.zoombit.repository.MarketRepository;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
@@ -14,16 +17,26 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MarketService {
 
+    public static final Logger logger = LoggerFactory.getLogger(MarketService.class.getName());
+
     @Autowired
     private MarketRepository marketRepository;
 
-    public void saveAllMarkets() throws Exception {
+    public void saveAllMarkets() {
+
         RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
         String url = "https://api.bithumb.com/v1/market/all?isDetails=false";
         String response = restTemplate.getForObject(url, String.class);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        List<MarketDTO> marketDtos = objectMapper.readValue(response, new TypeReference<List<MarketDTO>>() {});
+        List<MarketDTO> marketDtos = null;
+
+        try {
+            marketDtos = objectMapper.readValue(response, new TypeReference<List<MarketDTO>>() {});
+        } catch (JsonProcessingException e) {
+            logger.error("JSON 처리 중 오류 발생: " + e.getMessage());
+            throw new RuntimeException("JSON 데이터 처리 실패", e);
+        }
 
         for (MarketDTO dto : marketDtos) {
             Markets market = new Markets();
@@ -33,7 +46,6 @@ public class MarketService {
             marketRepository.save(market);
         }
 
-        System.out.println("All markets saved successfully!");
     }
 
 }
